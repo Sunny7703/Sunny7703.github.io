5679bf483868794025521c0fe7a051e0514d8fbc
Sunny7703 <woow19960717@gmail.com>
2026-06-24 23:06:46 +0800

    Add files via upload

---
+++ On-Site_Examinations/Week4_01_UVa12503/12503-fail.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> move(n + 1); // move[i] = +1 or -1 for instruction i
+        int pos = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            string cmd;
+            cin >> cmd;
+
+        }
+
+    }
+    return 0;
+}
