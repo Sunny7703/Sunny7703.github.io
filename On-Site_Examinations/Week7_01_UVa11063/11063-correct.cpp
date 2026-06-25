525af585cc39336156469c81d280567ee03fa0ab
Sunny7703 <woow19960717@gmail.com>
2026-06-25 13:33:42 +0800

    Add files via upload

---
+++ On-Site_Examinations/Week7_01_UVa11063/11063-correct.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int caseNum = 1;
+    while(cin >> n)
+    {
+        if(n < 2)
+            break;
+
+        int b[100] = {};
+        for(int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+
+        bool ok = true;
+
+        for(int i = 1; i < n; i++)
+        {
+            if(b[i] <= b[i - 1])
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        int sum[20005] = {};
+
+        for(int i = 0; i < n && ok; i++)
+        {
+            for(int j = n - 1; j >= i; j--)
+            {
+                int s = b[i] + b[j];
+
+                if(sum[s])
+                {
+                    ok = false;
+                    break;
+                }
+
+                sum[s] = 1;
+            }
+        }
+
+        cout << "Case #" << caseNum++;
+        if(!ok)
+            cout << ": It is not a B2-Sequence." << endl;
+        else
+            cout << ": It is a B2-Sequence." << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
