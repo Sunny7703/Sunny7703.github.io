b02d7874acbd2cb41851ed1c1493d1bf7d76cb56
Sunny7703 <woow19960717@gmail.com>
2026-06-25 21:23:34 +0800

    Add files via upload

---
+++ Before-Class_Exercises/Week9_01_LeetCode724/lc724-fail.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+        for (int x : nums) total += x;
+        int left = 0;
+
+
+
+        return -1;
+    }
+};
