75976c9c0231d05568b4fe2459169b46d205492c
Sunny7703 <woow19960717@gmail.com>
2026-06-25 15:44:16 +0800

    Add files via upload

---
+++ Before-Class_Exercises/Week12_01_LeetCode1022/lc1022-fail.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int helper(TreeNode* node, int currentVal)
+    {
+        if (node == nullptr) return currentVal;
+        currentVal = currentVal * 2 + node->val;
+        if (node->left == nullptr && node->right == nullptr) return currentVal;
+        return helper(node->left, currentVal) + helper(node->right, currentVal);
+    }
+    int sumRootToLeaf(TreeNode* root)
+    {
+        return helper(root, 0);
+    }
+};
